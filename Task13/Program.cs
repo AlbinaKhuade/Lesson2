f4a4a9854ea665b1212db0ec9b6887e9884b4ee1
Albina <albine89@mail.ru>
2022-10-05 18:50:19 +0300

    Task13 correct

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -5,28 +5,15 @@
 
 Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-int temp = number; // используем временную переменную, чтобы в результате вывести изначально введенное число
 
-if (temp > 99) // добавить temp < -99 если не только для положительных чисел - но все равно неверно ?????????????
+
+if (number > 99)
 {
-    while (temp > 1000)
+    while (number > 1000)
     {
-        temp = temp / 10;
+        number = number / 10;
     }
-int thirdDigit = temp % 10;
-Console.WriteLine($"{number} -> {thirdDigit}");
+int thirdDigit = number % 10;
+Console.WriteLine($"Третья цифра введенного числа: {thirdDigit}");
 }
-else Console.WriteLine("Третьей цифры нет");
-
-
-
-// int SecondDigit(int num)
-// {
-//     int firstDigit = num % 100;
-//     int thirdDigit = num % 10;
-//     return (firstDigit - thirdDigit)/10;  // метод возращает результат вычисления
-
-// }
-
-// int result = SecondDigit(number);
-// Console.WriteLine($"{number} -> {result}");
+else Console.WriteLine($"Третьей цифры нет");
