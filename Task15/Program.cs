7e703e723360ed3ef5099c3ff4bc013cf1ac0557
Albina <albine89@mail.ru>
2022-10-07 19:53:38 +0300

    Task15 correct2

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -9,6 +9,21 @@
 Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-if (number == 6 || number == 7) Console.WriteLine("да");
-else if (number>0 && number<6) Console.WriteLine("нет");
-else Console.WriteLine("Такого дня недели не существует");
+// if (number == 6 || number == 7) Console.WriteLine("да");
+// else if (number>0 && number<6) Console.WriteLine("нет");
+// else Console.WriteLine("Такого дня недели не существует");
+
+
+bool Weekend(int num)
+{
+    if (num == 6 || number == 7) return true;
+    else return false;
+}
+
+if (number < 1 || number > 7) Console.WriteLine("Такого дня недели не существует");
+else
+{
+    bool res = Weekend(number);
+    if (res == true) Console.WriteLine("да");
+    else Console.WriteLine("нет");
+}
